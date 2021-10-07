f564e4a19d95a9b9d2be2cccbb6eeae279dc6b63
Evan Nixon <enixon@121systems.com>
2021-10-07 12:06:37 +0100

    Customizations for internal use.

--- chrome-extension/content_script.js
+++ chrome-extension/content_script.js
@@ -1,27 +1,40 @@
 
 window.addEventListener('click', evt => {
-	// ユーザーの操作によるイベントならisTrusted == true
-	// If event is fired by user's operation then isTrusted == true.
-	// Chrome 46.0～
-	// https://developer.mozilla.org/ja/docs/Web/API/Event/isTrusted
-	if (!evt.isTrusted) return;
-	let target = evt.target;
-	while (target && target.tagName.toLowerCase() !== 'a') {
-		target = target.parentElement;
-	}
-	if (target) {
-		// check for baseVal of svg a tag's href-SVGAnimatedString
-		const url = target instanceof SVGAElement ? target.href.baseVal : target.href;
-		if (url.startsWith('file://')) {
-			evt.preventDefault();
-			// 拡張が再読み込みされた場合エラーになるので捕捉
-			// Catch the error for the extension is reloaded.
-			try {
-				chrome.runtime.sendMessage({
-					method: 'openLocalFile',
-					localFileUrl: url,
-				});
-			} catch (e) {}
-		}
-	}
+    // If event is fired by user's operation then isTrusted == true.
+    // Requires Chrome 46.0 or Greater
+    // https://developer.mozilla.org/ja/docs/Web/API/Event/isTrusted
+    if (!evt.isTrusted) return;
+    let target = evt.target;
+    while (target && target.tagName.toLowerCase() !== 'a' && target.tagName.toLowerCase() !== 'area') {
+        target = target.parentElement;
+    }
+
+
+    if (target) {
+        // check for baseVal of svg a tag's href-SVGAnimatedString
+        const url = target instanceof SVGAElement ? target.href.baseVal : target.href;
+        if (url.startsWith('file://')) if (url.includes('.121systems.com')) {
+            evt.preventDefault();
+            // Catch the about:blank#blocked default behaviour
+            try {
+                chrome.runtime.sendMessage({
+                    method: 'openLocalFile',
+                    localFileUrl: url,
+                });
+            } catch (e) { }
+        } else if (url.startsWith('file://192.168.')) {
+            evt.preventDefault();
+            // Catch the about:blank#blocked default behaviour
+            try {
+                chrome.runtime.sendMessage({
+                    method: 'openLocalFile',
+                    localFileUrl: url,
+                });
+            } catch (e) { }
+        }
+
+    }
+
+}, {
+    capture: true,
 });
