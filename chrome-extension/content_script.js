830075ce7aaed70ccb46cfb0ec03c8650ddf5d0a
evan-annix <evan.annix@gmail.com>
2021-10-12 16:28:38 +0000

    Started new http:// listener function

--- chrome-extension/content_script.js
+++ chrome-extension/content_script.js
@@ -13,6 +13,14 @@ window.addEventListener('click', evt => {
     if (target) {
         // check for baseVal of svg a tag's href-SVGAnimatedString
         const url = target instanceof SVGAElement ? target.href.baseVal : target.href;
+        
+        //New function to listen for clicks which contain http and append or remove http for file link functionality
+        if (url.startsWith('http://file://'))
+            try {
+                
+            }
+
+
         if (url.startsWith('file://')) if (url.includes('.121systems.com')) {
             evt.preventDefault();
             // Catch the about:blank#blocked default behaviour
@@ -31,10 +39,20 @@ window.addEventListener('click', evt => {
                     localFileUrl: url,
                 });
             } catch (e) { }
-        }
+        } else if (url.startsWith('file://srvfps')) {
+			evt.preventDefault();
+			// Catch the about:blank#blocked default behaviour
+			try {
+				chrome.runtime.sendMessage({
+					method: 'openLocalFile',
+					localFileUrl: url,
+				});
+			} catch (e) {}
+		} 
 
     }
 
 }, {
     capture: true,
-});
+})
+;
